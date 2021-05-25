1d17df87eb501d359757a5b4c04ce07f2888390e
Yüksel Can Özdemir <yukselcanozdemir@gmail.com>
2021-05-25 21:40:19 +0300

    commit 2
    swagger added

--- hrms/src/main/java/com/kodlama/io/hrms/entities/concrete/JobTitle.java
+++ hrms/src/main/java/com/kodlama/io/hrms/entities/concrete/JobTitle.java
@@ -23,9 +23,6 @@ public class JobTitle {
 	@Column(name="id")
 	private int id;
 
-	@Column(name="user_id")
-	private int userId;
-
-	@Column(name="name")
-	private String 	name;
+	@Column(name="title")
+	private String title;
 }
